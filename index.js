cbb2f4c020e4c21e1f5239e2a72d1ed009bf23bc
asmaa.abbas@wework.com <asmaa.abbas@wework.com>
2017-12-25 17:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,16 @@ const divide = (a, b) => {
 
 const inc = (a) => {
   return a += 1;
-}
+};
 
 const dec = (a) => {
   return a -= 1;
-}
+};
+
+const makeInt = (n) => {
+  return parseInt(n, 10);
+};
+
+const preserveDecimal = (n) => {
+  return parseFloat(n);
+};
